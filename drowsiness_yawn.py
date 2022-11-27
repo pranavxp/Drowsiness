0eb2b3c7ed15e516e36e90120d5b70665581d0be
Pranav P <pranav.pramod@tutanota.com>
2022-11-27 10:24:35 +0530

    Update drowsiness_yawn.py

--- drowsiness_yawn.py
+++ drowsiness_yawn.py
@@ -70,7 +70,7 @@ ap.add_argument("-s", "--source", type=int, default=0,
                 help="index of webcam on system")
 args = vars(ap.parse_args())
 
-EYE_AR_THRESH = 0.3
+EYE_AR_THRESH = 0.4
 EYE_AR_CONSEC_FRAMES = 30
 YAWN_THRESH = 20
 alarm_status = False
